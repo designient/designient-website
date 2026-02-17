595f7d0df6c677339f82a89b1ff4002374d1714c
Moaisus <sameer@moaisus.com>
2026-02-17 08:18:44 +0530

    fix(api): change contact route runtime to nodejs to fix build error

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from 'next/server'
-export const runtime = 'edge'
+export const runtime = 'nodejs'
 import { contactLeadEmail, contactCustomerCopy } from '../../../lib/email-templates'
 import { getResendFrom } from '../../../lib/resend'
 
